3822732920c7d6ed352c0dc1c6a5ecb127751af4
Mochammad Hanif R <etrnal70@gmail.com>
2022-06-18 04:26:29 +0700

    driver: simplify schema (#10)
    remove image struct and use struct fields for image
    also add missing many2many schema for reply

--- drivers/databases/thread/record.go
+++ drivers/databases/thread/record.go
@@ -12,21 +12,15 @@ type Thread struct {
 	TopicID  uint
 	Topic    topic.Topic
 	AuthorID uint
-	Author   user.User    `gorm:"ForeignKey:AuthorID"`
+	Author   user.User `gorm:"ForeignKey:AuthorID"`
 	Title    string
 	Content  string
-	Image    *ThreadImage `gorm:"contraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Image1   *string
+	Image2   *string
+	Image3   *string
+	Image4   *string
+	Image5   *string
 
-  LikedBy []*user.User `gorm:"many2many:liked_thread"`
-  UnlikedBy []*user.User `gorm:"many2many:unliked_thread"`
-}
-
-type ThreadImage struct {
-	gorm.Model
-	ThreadID  uint
-	ImageURL1 string
-	ImageURL2 string
-	ImageURL3 string
-	ImageURL4 string
-	ImageURL5 string
+	LikedBy   []*user.User `gorm:"many2many:liked_thread"`
+	UnlikedBy []*user.User `gorm:"many2many:unliked_thread"`
 }
