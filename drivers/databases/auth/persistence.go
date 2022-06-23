b47e84c7bb5aadaec082f0b063c96a129022412a
Mochammad Hanif R <etrnal70@gmail.com>
2022-06-23 09:48:04 +0700

    domain(auth): Implement auth service

---
+++ drivers/databases/auth/persistence.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"fgd/app/middleware"
+	"fgd/core/auth"
+
+	"gorm.io/gorm"
+)
+
+type persistenceAuthRepository struct {
+	Conn *gorm.DB
+}
+
+func (rp *persistenceAuthRepository) DeleteAuth(userId int) error {
+	return rp.Conn.Where("user_id = ?", userId).Delete(&Auth{}).Error
+}
+
+func (rp *persistenceAuthRepository) FetchAuth(userId int) (auth.Domain, error) {
+	auth := Auth{}
+	res := rp.Conn.Where("user_id = ?", userId).Take(&auth)
+	return auth.toDomain(), res.Error
+}
+
+func (rp *persistenceAuthRepository) StoreAuth(userId int, auth middleware.CustomToken) error {
+	authStore := Auth{
+		UserID:      uint(userId),
+		AccessUUID:  auth.AccessToken,
+		RefreshUUID: auth.RefreshUUID,
+	}
+
+	res := rp.Conn.Create(&authStore)
+
+	return res.Error
+}
+
+func InitPersistenceAuthRepository(c *gorm.DB) auth.Repository {
+	return &persistenceAuthRepository{
+		Conn: c,
+	}
+}
