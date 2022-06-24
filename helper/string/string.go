031f76d143d604e647117516b69df4e2ad8f447f
Mochammad Hanif R <etrnal70@gmail.com>
2022-06-24 15:43:14 +0700

    helper: Implement random 8 digit generator

---
+++ helper/string/string.go
@@ -0,0 +1,16 @@
+package string
+
+import (
+	"crypto/rand"
+	"math/big"
+	mrand "math/rand"
+	"strconv"
+	"time"
+)
+
+func GenerateRandomNumber() string {
+	mrand.Seed(time.Now().Unix())
+	max := big.NewInt(99999999)
+	n, _ := rand.Int(rand.Reader, max)
+	return strconv.Itoa(int(n.Int64()))
+}
