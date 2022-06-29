f15d2e071aca454dfa6f7a610fb6ec4786a636ac
Mochammad Hanif R <etrnal70@gmail.com>
2022-06-29 16:16:10 +0700

    fix(controllers/thread): Add missing response data

--- controllers/thread/response/json.go
+++ controllers/thread/response/json.go
@@ -6,33 +6,45 @@ import (
 )
 
 type Thread struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Content     string    `json:"content,omitempty"`
-	Image1      string    `json:"image_1,omitempty"`
-	Image2      string    `json:"image_2,omitempty"`
-	Image3      string    `json:"image_3,omitempty"`
-	Image4      string    `json:"image_4,omitempty"`
-	Image5      string    `json:"image_5,omitempty"`
-	LikeCount   int       `json:"like_count"`
-	UnlikeCount int       `json:"unlike_count"`
-	ReplyCount  int       `json:"reply_count"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID           int          `json:"id"`
+	Author       ThreadAuthor `json:"author"`
+	Title        string       `json:"title"`
+	Content      string       `json:"content,omitempty"`
+	Image1       string       `json:"image_1,omitempty"`
+	Image2       string       `json:"image_2,omitempty"`
+	Image3       string       `json:"image_3,omitempty"`
+	Image4       string       `json:"image_4,omitempty"`
+	Image5       string       `json:"image_5,omitempty"`
+	LikedCount   int          `json:"liked_count"`
+	UnlikedCount int          `json:"unliked_count"`
+	ReplyCount   int          `json:"reply_count"`
+	UpdatedAt    time.Time    `json:"updated_at"`
+}
+
+type ThreadAuthor struct {
+	ID           int     `json:"id"`
+	Username     string  `json:"username"`
+	ProfileImage *string `json:"profile_image,omitempty"`
 }
 
 func FromDomain(data *thread.Domain) Thread {
 	return Thread{
-		ID:          data.ID,
-		Title:       data.Title,
-		Content:     data.Content,
-		Image1:      *data.Image1,
-		Image2:      *data.Image2,
-		Image3:      *data.Image3,
-		Image4:      *data.Image4,
-		Image5:      *data.Image5,
-		LikeCount:   data.LikeCount,
-		UnlikeCount: data.UnlikeCount,
-		ReplyCount:  data.ReplyCount,
-		UpdatedAt:   data.UpdatedAt,
+		ID: data.ID,
+		Author: ThreadAuthor{
+			ID:           data.Author.ID,
+			Username:     data.Author.Username,
+			ProfileImage: &data.Author.ProfileImage,
+		},
+		Title:        data.Title,
+		Content:      data.Content,
+		Image1:       *data.Image1,
+		Image2:       *data.Image2,
+		Image3:       *data.Image3,
+		Image4:       *data.Image4,
+		Image5:       *data.Image5,
+		LikedCount:   data.LikeCount,
+		UnlikedCount: data.UnlikeCount,
+		ReplyCount:   data.ReplyCount,
+		UpdatedAt:    data.UpdatedAt,
 	}
 }
