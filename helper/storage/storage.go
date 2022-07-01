33929a84e6b8e9ce56e1689ab6f00244cd0ac5d0
Mochammad Hanif R <etrnal70@gmail.com>
2022-07-01 13:31:58 +0700

    helper: Implement storage helper

---
+++ helper/storage/storage.go
@@ -0,0 +1,31 @@
+package storage
+
+import (
+	"io"
+	"mime/multipart"
+	"os"
+
+	"github.com/google/uuid"
+)
+
+func StoreFile(file *multipart.FileHeader) (string, error) {
+	fileName := uuid.New().String()
+
+	src, err := file.Open()
+	if err != nil {
+		return "", err
+	}
+
+	defer src.Close()
+
+	dst, err := os.Create(fileName)
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return "", err
+	}
+
+	return fileName, nil
+}
