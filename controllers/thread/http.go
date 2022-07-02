5a53cdbf49b16a165c66386a32f10714fc70a3dd
Mochammad Hanif R <etrnal70@gmail.com>
2022-07-02 09:26:32 +0700

    fix(controllers/thread): Use proper multipart/form-data implementation

--- controllers/thread/http.go
+++ controllers/thread/http.go
@@ -6,15 +6,14 @@ import (
 	"fgd/controllers/thread/request"
 	"fgd/controllers/thread/response"
 	"fgd/core/thread"
+	"fgd/helper/storage"
 	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
-type ThreadController struct {
-	threadUsecase thread.Usecase
-}
+type ThreadController struct{ threadUsecase thread.Usecase }
 
 func InitThreadController(tc thread.Usecase) *ThreadController {
 	return &ThreadController{
@@ -23,25 +22,103 @@ func InitThreadController(tc thread.Usecase) *ThreadController {
 }
 
 func (cr *ThreadController) Create(c echo.Context) error {
+	claims := middleware.ExtractUserClaims(c)
+
+	var image1 string
+	var image2 string
+	var image3 string
+	var image4 string
+	var image5 string
+
+	title := c.FormValue("title")
+	content := c.FormValue("content")
+
+	newThread := request.Thread{}
+
+	newThread.Title = title
+	newThread.Content = content
+
+	imageFile1, err := c.FormFile("image_1")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image1, uploadErr := storage.StoreFile(imageFile1)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		newThread.Image1 = image1
+	} else {
+		newThread.Image1 = image1
+	}
+
+	imageFile2, err := c.FormFile("image_2")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image2, uploadErr := storage.StoreFile(imageFile2)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		newThread.Image2 = image2
+	} else {
+		newThread.Image2 = image2
+	}
+
+	imageFile3, err := c.FormFile("image_3")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image3, uploadErr := storage.StoreFile(imageFile3)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		newThread.Image3 = image3
+	} else {
+		newThread.Image3 = image3
+	}
+
+	imageFile4, err := c.FormFile("image_4")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image4, uploadErr := storage.StoreFile(imageFile4)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		newThread.Image4 = image4
+	} else {
+		newThread.Image4 = image4
+	}
+
+	imageFile5, err := c.FormFile("image_5")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image5, uploadErr := storage.StoreFile(imageFile5)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		newThread.Image5 = image5
+	} else {
+		newThread.Image5 = image5
+	}
+
 	topicId, err := strconv.Atoi(c.Param("topicId"))
 	if err != nil {
 		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
 	}
 
-	claims := middleware.ExtractUserClaims(c)
-
-	thread := request.Thread{}
-	err = c.Bind(&thread)
-	if err != nil {
-		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
-	}
-
-	newThread, err := cr.threadUsecase.CreateThread(thread.ToDomain(), claims.UserID, topicId)
+	threadDomain, err := cr.threadUsecase.CreateThread(newThread.ToDomain(), claims.UserID, topicId)
 	if err != nil {
 		return controllers.FailureResponse(c, http.StatusInternalServerError, err.Error())
 	}
 
-	return controllers.SuccessResponse(c, http.StatusCreated, response.FromDomain(&newThread))
+	return controllers.SuccessResponse(c, http.StatusCreated, response.FromDomain(&threadDomain))
 }
 
 func (cr *ThreadController) Update(c echo.Context) error {
@@ -52,18 +129,101 @@ func (cr *ThreadController) Update(c echo.Context) error {
 
 	claims := middleware.ExtractUserClaims(c)
 
-	thread := request.Thread{}
-	err = c.Bind(&thread)
+	var image1 string
+	var image2 string
+	var image3 string
+	var image4 string
+	var image5 string
+
+	title := c.FormValue("title")
+	content := c.FormValue("content")
+
+	updatedThread := request.Thread{}
+
+	updatedThread.Title = title
+	updatedThread.Content = content
+
+	imageFile1, err := c.FormFile("image_1")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image1, uploadErr := storage.StoreFile(imageFile1)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		updatedThread.Image1 = image1
+	} else {
+		updatedThread.Image1 = image1
+	}
+
+	imageFile2, err := c.FormFile("image_2")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image2, uploadErr := storage.StoreFile(imageFile2)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		updatedThread.Image2 = image2
+	} else {
+		updatedThread.Image2 = image2
+	}
+
+	imageFile3, err := c.FormFile("image_3")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image3, uploadErr := storage.StoreFile(imageFile3)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		updatedThread.Image3 = image3
+	} else {
+		updatedThread.Image3 = image3
+	}
+
+	imageFile4, err := c.FormFile("image_4")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image4, uploadErr := storage.StoreFile(imageFile4)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		updatedThread.Image4 = image4
+	} else {
+		updatedThread.Image4 = image4
+	}
+
+	imageFile5, err := c.FormFile("image_5")
+	if err != nil && err != http.ErrMissingFile {
+		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
+	}
+	if err != http.ErrMissingFile {
+		image5, uploadErr := storage.StoreFile(imageFile5)
+		if uploadErr != nil {
+			return controllers.FailureResponse(c, http.StatusUnprocessableEntity, uploadErr.Error())
+		}
+		updatedThread.Image5 = image5
+	} else {
+		updatedThread.Image5 = image5
+	}
+
+	err = c.Bind(&updatedThread)
 	if err != nil {
 		return controllers.FailureResponse(c, http.StatusBadRequest, err.Error())
 	}
 
-	updatedThread, err := cr.threadUsecase.UpdateThread(thread.ToDomain(), claims.UserID, threadId)
+	threadDomain, err := cr.threadUsecase.UpdateThread(updatedThread.ToDomain(), claims.UserID, threadId)
 	if err != nil {
 		return controllers.FailureResponse(c, http.StatusInternalServerError, err.Error())
 	}
 
-	return controllers.SuccessResponse(c, http.StatusOK, response.FromDomain(&updatedThread))
+	return controllers.SuccessResponse(c, http.StatusOK, response.FromDomain(&threadDomain))
 }
 
 func (cr *ThreadController) Delete(c echo.Context) error {
