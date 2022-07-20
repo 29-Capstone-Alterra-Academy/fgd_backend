46349a6d819f4d3abcaad3a32996830f17a588ba
Mochammad Hanif R <etrnal70@gmail.com>
2022-07-20 07:01:18 +0700

    fix(drivers): enable sslmode with provided cert
    Needed for new db hosting

--- drivers/persistence/driver.go
+++ drivers/persistence/driver.go
@@ -19,7 +19,7 @@ type PersistenceConfig struct {
 }
 
 func (c *PersistenceConfig) InitPersistenceDB() *gorm.DB {
-	connString := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta",
+	connString := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=verify-ca sslrootcert=ca.pem TimeZone=Asia/Jakarta",
 		c.Username,
 		c.Password,
 		c.Host,
